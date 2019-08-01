323d036a0a978f82c9e5cd0b97fc60319e225e35
yuanzhenxia <1097757391@qq.com>
2019-08-01 21:08:28 +0800

    one day

---
+++ src/views/login/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <el-card class="my-card">
+      <div slot="header" class="clearfix">
+        <img src="../../assets/images/logo_index.png" alt />
+      </div>
+    </el-card>
+  </div>
+</template>
+<script>
+export default {}
+</script>
+<style scope lang='less'>
+.container {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: url(../../assets/images/login_bg.jpg) no-repeat center/cover;
+  .my-card {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -60%);
+    width: 400px;
+    height: 300px;
+    img {
+      display: block;
+      margin: 0 auto;
+    }
+  }
+}
+</style>
