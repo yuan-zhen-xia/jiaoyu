1a77b63d3bf676cfe4ba0af948b6932b2111a9f3
yuanzhenxia <1097757391@qq.com>
2019-08-05 21:50:37 +0800

    内容静态页面搭建完成

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -5,7 +5,7 @@
       <!-- 卡片分层 -->
       <div slot="header">
         <!-- 面包屑摸板 -->
-          <my-bread>内容管理</my-bread>
+        <my-bread>内容管理</my-bread>
       </div>
       <el-form label-width="80px" size="small">
         <el-form-item label="状态:">
@@ -32,13 +32,13 @@
         </el-form-item>
         <el-form-item label="日期:">
           <!-- 日期 -->
-           <el-date-picker
-      v-model="dataArr"
-      type="daterange"
-      range-separator="至"
-      start-placeholder="开始日期"
-      end-placeholder="结束日期">
-    </el-date-picker>
+          <el-date-picker
+            v-model="dataArr"
+            type="daterange"
+            range-separator="至"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+          ></el-date-picker>
         </el-form-item>
         <el-form-item>
           <el-button type="primary">筛选</el-button>
@@ -46,12 +46,28 @@
       </el-form>
     </el-card>
     <!-- 筛选结果 -->
+    <el-card>
+      <div slot="header">根据筛选条件查询到条结果</div>
+      <!-- 表格组件 -->
+      <el-table :data="tableData">
+        <!-- prop是指定字段显示该字段的值  -->
+        <el-table-column prop="date" label="封面" width="180"></el-table-column>
+        <el-table-column prop="name" label="姓名" width="180"></el-table-column>
+        <el-table-column prop="address" label="地址"></el-table-column>
+      </el-table>
+      <!-- 分页组件 -->
+      <div style="text-align: center;margin-top:30px">
+         <el-pagination background layout="prev, pager, next" :total="1000"></el-pagination>
+      </div>
+
+    </el-card>
   </div>
 </template>
 <script>
 // 引入封装好的面包屑组件
 export default {
   // 使用面包屑组件
+
   data () {
     return {
       // 筛选表单数据，提交给后台的参数
@@ -60,14 +76,23 @@ export default {
         channel_id: null
       },
       // 频道下拉数据，不需要提交到后台
-      channelOptions: [{
-        value: 1,
-        label: 'js'
-      }],
+      channelOptions: [
+        {
+          value: 1,
+          label: 'js'
+        }
+      ],
       // 日期数据
-      dataArr: []
+      dataArr: [],
+      // 定义表格数据
+      articles: [{}]
     }
   }
 }
 </script>
-<style scoped lang='less'></style>
+<style scoped lang='less'>
+.el-card {
+  margin-top: 20px;
+
+}
+</style>
